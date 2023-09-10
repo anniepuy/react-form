d21b9c22433c87d671e18a282c92c610652b2443
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-09-10 10:55:19 -0500

    adding global css

---
+++ src/components/Form.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <form>
+        <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input type="text" className="form-control"/>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="age" className="form-label">Age</label>
+            <input type="number" className="form-control"/>
+        </div>
+        
+    </form>
+  )
+}
+
+export default Form
