157e2ec841b4f02153f859bf2fff02385583d28b
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-09-10 11:00:39 -0500

    adding handleSubmit to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, { FormEvent } from "react";
 
 const Form = () => {
-  return (
-    <form onSubmit={(event) => {
+    const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
         console.log('Submit');
-    }}>
-        <div className="mb-3">
-            <label htmlFor="name" className="form-label">Name</label>
-            <input type="text" className="form-control"/>
-        </div>
-        <div className="mb-3">
-            <label htmlFor="age" className="form-label">Age</label>
-            <input type="number" className="form-control"/>
-        </div>
-        <button className="btn btn-primary" type="submit">Submit</button>
-    </form>
-  )
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label">Name</label>
+                <input type="text" className="form-control"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="age" className="form-label">Age</label>
+                <input type="number" className="form-control"/>
+            </div>
+            <button className="btn btn-primary" type="submit">Submit</button>
+        </form>
+    )
 }
 
 export default Form
